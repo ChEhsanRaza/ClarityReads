e7130e3afe0c3d902b23365c8f512d371f774099
ChEhsanRaza <162859278+ChEhsanRaza@users.noreply.github.com>
2024-10-19 13:14:01 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript for joining the WhatsApp channel
+document.getElementById('joinButton').addEventListener('click', function() {
+    window.open('https://chat.whatsapp.com/YourWhatsAppLinkHere', '_blank');
+});
